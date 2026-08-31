4e7e2518d2f9db44c8fe0f02f07a89f69cf22eba
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:49 +0300

    Add reports page

---
+++ src/app/raporlar/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Raporlar' subtitle='Servis performansı ve finansal analizler' action='Rapor Al'/>}
