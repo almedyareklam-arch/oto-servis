703777c36b4d1ec69d8488f622f4f8b82a978d30
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:13:02 +0300

    Add suppliers page

---
+++ src/app/tedarikciler/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Tedarikçiler' subtitle='Tedarikçi hesapları ve satın alma ilişkileri' action='+ Yeni Tedarikçi'/>}
