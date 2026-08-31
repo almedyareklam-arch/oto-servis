6684244ca4ed91ba30ab3b00ea4a2f2d96851bfc
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:13:07 +0300

    Add quotations page

---
+++ src/app/teklifler/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Teklifler' subtitle='Müşteri teklifleri ve onay süreçleri' action='+ Teklif Oluştur'/>}
