2d7aa032d310f0b81fb8d56d6d145062d5f486f6
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:12 +0300

    Add vehicles page

---
+++ src/app/araclar/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Araçlar' subtitle='Araç kartları ve servis geçmişi' action='+ Yeni Araç'/>}
