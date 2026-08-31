37c907ce70c330ee5b8801636e2a05d1e11504cb
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:36 +0300

    Add customers page

---
+++ src/app/musteriler/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Müşteriler' subtitle='Müşteri kayıtları ve servis ilişkileri' action='+ Yeni Müşteri'/>}
