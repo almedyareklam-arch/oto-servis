4adb620dcb4cd3dc9157d805542bffb5298d0b19
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:31 +0300

    Add cash ledger page

---
+++ src/app/kasa-cari/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Kasa & Cari' subtitle='Kasa hareketleri, alacak ve borç takibi' action='+ Kasa Hareketi'/>}
