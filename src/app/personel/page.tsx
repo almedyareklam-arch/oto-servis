afe735d9e9ba1e58b4acc736f07843c3b2469999
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:40 +0300

    Add staff page

---
+++ src/app/personel/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Personel / Ustalar' subtitle='Ekip, görev ve performans yönetimi' action='+ Yeni Personel'/>}
