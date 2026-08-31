e2021fc544923c2b2ac607004468f6c468d37f65
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:45 +0300

    Add appointments page

---
+++ src/app/randevular/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Randevular' subtitle='Servis takvimi ve yaklaşan randevular' action='+ Yeni Randevu'/>}
