6d13f6ce5a355834cad5b3da5a4d000df2725ccb
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:17 +0300

    Add settings page

---
+++ src/app/ayarlar/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Ayarlar' subtitle='Firma, şube ve sistem tercihleri' action='Değişiklikleri Kaydet'/>}
