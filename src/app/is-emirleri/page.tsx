0c0f643cf53c7198882d3ca5c743100c5fc846d5
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:27 +0300

    Add work orders page

---
+++ src/app/is-emirleri/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='İş Emirleri' subtitle='Atölyedeki tüm işleri tek ekrandan yönetin' action='+ Yeni İş Emri'/>}
