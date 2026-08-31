771f28ce66e525a292080a7b8b155f718cb9790f
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:58 +0300

    Add inventory page

---
+++ src/app/stok/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Stok / Yedek Parça' subtitle='Stok seviyeleri, parçalar ve hareketler' action='+ Stok Girişi'/>}
