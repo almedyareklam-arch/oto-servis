d8f4a32f9124a252721cb3f27c495cfe23f9edb6
almedyareklam-arch <almedyareklam@gmail.com>
2026-08-31 23:12:23 +0300

    Add invoices page

---
+++ src/app/faturalar/page.tsx
@@ -0,0 +1,2 @@
+import {ModulePage} from '@/components/ModulePage';
+export default function Page(){return <ModulePage title='Faturalar' subtitle='Fatura ve tahsilat durumlarını yönetin' action='+ Yeni Fatura'/>}
